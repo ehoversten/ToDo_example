6dc2758bb47feb495e8fc5e5794eab421c0447b7
ehoversten <ehoversten@gmail.com>
2018-11-27 16:32:55 -0500

    added ADD todo Item form

--- app.js
+++ app.js
@@ -19,8 +19,11 @@ app.set('views', path.join(__dirname, './views'))
 
 
 
+
 // Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
+
+// #### DATABASE CONNECTION #####
 
 // CONNECT TO MONGOOSE DB
 mongoose.connect('mongodb://localhost/todo-dev', {
@@ -30,23 +33,41 @@ mongoose.connect('mongodb://localhost/todo-dev', {
     .catch(err => console.log(err));
 
 
+
+// #### MODELS #####
+
+// Load Item Model
+require('./models/Item')
+const Todo = mongoose.model('todos');
+
+
+// #### MIDDLEWARE #####
+
 // Handlebars Middleware
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+
+// #### ROUTING #####
+
 // Index Route
 app.get('/', (req, res) => {
-
     const title = 'Welcome';
-
     res.render('index', { title:title })
-})
+});
 
 // About Route
 app.get('/about', (req, res) => {
     res.render('about')
+});
 
-})
+// Add Todo Item Form
+app.get('/items/add', (req, res) => {
+    res.render('items/add')
+});
+
+
+
 
 // tell the express app to listen on port 8000, always put this at the end of your server.js file
 app.listen(5000, function () {
---
+++ models/Item.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+
+// Create Schema 
+const ItemSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    }, 
+    details: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+mongoose.model('todos', ItemSchema);
+
